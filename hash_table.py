caaaccde9d67db7763295bdd35ce8b2c5bc2ae8e
trandinhthang203 <tranthangkhuong203@gmail.com>
2025-08-26 23:51:54 +0700

    feat: complete solve Roman to Interger: from chatGPT:v

--- hash_table.py
+++ hash_table.py
@@ -1,37 +1,19 @@
 class hash_table:
-    def romanToInt(self, s = 'MCMXCIV'):
-        """
-        :type s: str
-        :rtype: int
-        """
-        result = 0
-        left = 0
-        list_input = []
-        seen = ()
-
-        dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500, 'M': 1000
         }
-        for i in s:
-            if i in dict:
-                list_input.append(dict[i])
         
-        for index, right in enumerate(list_input, start=1):
-            while list_input[left] < right:
-                result += right - list_input[left]
-                del list_input[left:left+1]
-                left += 1
-                 
-            left += 1
-        result += sum(list_input)
-        return result
-
+        total = 0
+        for i in range(len(s) - 1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+        
+        total += roman[s[-1]]
+        return total
 
 h = hash_table()
-print(h.romanToInt())
+print(h.romanToInt('MCMXCIV'))

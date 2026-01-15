8f7a524dc74355a26df8349485f1b6a7434935a8
trandinhthang203 <tranthangkhuong203@gmail.com>
2026-01-16 02:15:33 +0700

    logging and exception eror

---
+++ projects/src/logger.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import logging
+from datetime import datetime
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+LOG_FILE = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.log")
+LOG_DIR = os.path.join(BASE_DIR, "logs", LOG_FILE)
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+logging.basicConfig(
+    filename=LOG_FILE,
+    format='[%(asctime)s] %(levelname)s - %(message)s',
+    level=logging.INFO
+
+)
+
+if __name__ == "__main__":
+    logging.info("Logging started")

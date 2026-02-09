7c1a0a82d7f4393f72e23c74add57bd4e874e0c2
trandinhthang203 <tranthangkhuong203@gmail.com>
2026-02-10 00:16:37 +0700

    inverse transform for scaler standard

--- projects/src/components/data_transformation.py
+++ projects/src/components/data_transformation.py
@@ -113,6 +113,8 @@ class DataTransformation:
                 ("processor", processor)
             ]
         )
+        # scaler = StandardScaler()
+        # scaler.inverse_transform(y_pred)
 
         return full_pipeline
         
